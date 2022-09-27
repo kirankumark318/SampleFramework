268c8e57e72c701017f212a604f8c705088bc95f
Hp <Hp@DESKTOP-SITFNC3>
2022-09-27 12:50:48 +0530

    First commit

--- src/test/java/GithubPackage/CreateTest.java
+++ src/test/java/GithubPackage/CreateTest.java
@@ -1,5 +1,11 @@
 package GithubPackage;
 
-public class CreateTest {
+import org.testng.annotations.Test;
 
+public class CreateTest {
+@Test
+public void create()
+{
+	System.out.println("-----created-----");
+}
 }
